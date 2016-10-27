b2a68df4ec2d2a0422533fa27189f6f4c6e70339
4oo4 <4oo4@users.noreply.github.com>
2016-10-26 20:02:54 -0500

    Create ArticleSearch.php
    Rebasing https://github.com/wallabag/wallabag/pull/1685/files to new branch for search testing

---
+++ src/Wallabag/CoreBundle/Entity/ArticleSearch.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wallabag\CoreBundle\Entity;
+
+class ArticleSearch
+{
+    private $searchTerm;
+    
+    public function getSearchTerm()
+    {
+        return $this->searchTerm;
+    }
+    
+    public function setSearchTerm($searchTerm)
+    {
+        $this->searchTerm = $searchTerm;
+    
+        return $this;
+    }
+}
